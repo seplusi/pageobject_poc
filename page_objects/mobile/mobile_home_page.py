858a5b5169086b62c8f9548e3411eb3d97a41061
Luis <seplusi_arcanjo@hotmail.com>
2025-05-22 15:53:07 +0200

    Fixed and improved mobile tests

--- page_objects/mobile/mobile_home_page.py
+++ page_objects/mobile/mobile_home_page.py
@@ -23,6 +23,23 @@ class mobileConvertHomePage(commonClass):
         # Accept cookie
         self.wait_driver.until(EC.visibility_of_element_located(self.locators['ACCEPT_COOKIE'])).click()
 
+    @property
+    def selected_source_currency(self):
+        return self.wait_driver.until(EC.visibility_of_element_located(self.locators['SOURCE_CURRENCY']))
+
+    @property
+    def selected_dst_currency(self):
+        return self.wait_driver.until(EC.visibility_of_element_located(self.locators['DEST_CURRENCY']))
+
+    @property
+    def conversion_text1(self):
+        return self.wait_driver.until(EC.visibility_of_element_located(self.locators['RESULT_1ST_LINE'])).text
+
+    @property
+    def conversion_text2(self):
+        return self.wait_driver.until(EC.visibility_of_element_located(self.locators['RESULT_2ND_LINE'])).text
+
+
     def insert_amount(self, amount):
         self.insert_amount_box.send_keys(Keys.BACKSPACE)
         self.insert_amount_box.send_keys(amount)
@@ -32,44 +49,49 @@ class mobileConvertHomePage(commonClass):
         # Click textbox and write currency name
         element = self.wait_driver.until(EC.visibility_of_element_located(self.locators['SOURCE_CURRENCY']))
         element.click()
+        element = self.wait_driver.until(EC.visibility_of_element_located(self.locators['CURRENCY_SEARCH_TXT']))
         element.send_keys(currency_name)
         for _ in range(10):
             try:
                 time.sleep(0.1)
                 # Wait for 1st element in list to be our currency
-                self.wait_driver.until(EC.visibility_of_element_located(self.locators['INSERT_SOURCE_CURRENCY_LST']))
-                currency = self.wait_driver.until(EC.visibility_of_element_located(self.locators['FIRST_CURRENCY']))
-                if currency_name.lower() in currency.text.lower():
-                    currency.click()
+                first_option_ele = self.wait_driver.until(EC.visibility_of_element_located(self.locators['FIRST_SRC_CURRENCY']))
+                if currency_name.lower() in first_option_ele.text.lower():
+                    first_option_ele.click()
                     break
             except StaleElementReferenceException:
                 continue
         else:
             assert False, f'Currency {currency_name} not found.'
-        # Press enter to finish selection
-        element.send_keys(Keys.ENTER)
+
 
     def select_dst_x(self, currency_name):
         # Click textbox and write currency name
-        element = self.wait_driver.until(EC.visibility_of_element_located(self.locators['INSERT_DST_CURRENCY']))
+        element = self.wait_driver.until(EC.visibility_of_element_located(self.locators['DEST_CURRENCY']))
         element.click()
+        element = self.wait_driver.until(EC.visibility_of_element_located(self.locators['CURRENCY_SEARCH_TXT']))
         element.send_keys(currency_name)
         for _ in range(10):
             try:
                 time.sleep(0.1)
                 # Wait for 1st element in list to be our currency
-                self.wait_driver.until(EC.visibility_of_element_located(self.locators['INSERT_DST_CURRENCY_LST']))
-                currency = self.wait_driver.until(EC.visibility_of_element_located(self.locators['FIRST_DST_CURRENCY']))
-                if currency_name.lower() in currency.text.lower():
-                    currency.click()
+                first_option_ele = self.wait_driver.until(EC.visibility_of_element_located(self.locators['FIRST_DST_CURRENCY']))
+                if currency_name.lower() in first_option_ele.text.lower():
+                    first_option_ele.click()
                     break
             except StaleElementReferenceException:
                 continue
         else:
             assert False, f'Currency {currency_name} not found.'
-        # Press enter to finish selection
-        element.send_keys(Keys.ENTER)
+
 
     def perform_conversion(self):
         self.wait_driver.until(EC.element_to_be_clickable(self.locators['CONVERT_BTN'])).click()
         self.wait_driver.until(EC.invisibility_of_element_located(self.locators['CONVERT_BTN']))
+
+    def get_ratings(self):
+        real2euro = self.wait_driver.until(EC.visibility_of_element_located(self.locators['BRL2EURO'])).text
+        self.real2euro = float(real2euro.split(' ')[-2])
+
+        euro2real = self.wait_driver.until(EC.visibility_of_element_located(self.locators['EURO2BRL'])).text
+        self.euro2real = float(euro2real.split(' ')[-2])
--- tests/test_ui.py
+++ tests/test_ui.py
@@ -2,6 +2,7 @@ import pytest
 from page_objects.home_page import convertHomePage
 from page_objects.mobile.mobile_home_page import mobileConvertHomePage
 from page_objects.mobile.calculator2_page import mobileCalcAppPage
+import re
 
 
 URL = 'https://www.xe.com/'
@@ -23,8 +24,37 @@ def test_webview_mobile_home_page(config, chrome_appiumriver):
     home_page = mobileConvertHomePage(chrome_appiumriver, config, URL)
 
     home_page.insert_amount(100)
-    #home_page.select_src_x('real')
+    
+    home_page.select_src_x('real')
+    assert 'real' in home_page.selected_source_currency.text.lower()
+
+    home_page.select_dst_x('euro')
+    assert 'euro' in home_page.selected_dst_currency.text.lower()
+
+    home_page.perform_conversion()
+    home_page.get_ratings()
+
+    assert home_page.conversion_text1 == "100.00 Brazilian Reais ="
+
+    partial_convertion = round(100*home_page.real2euro, 2)
+    re.search(f'{partial_convertion}[0-9]{3} Euros', home_page.conversion_text2)
 
 
 def test_native_app_calc(config, calc_appiumdriver):
-    mobileCalcAppPage(calc_appiumdriver, config, explicit_wait=5)
+    home_page = mobileCalcAppPage(calc_appiumdriver, config, explicit_wait=5)
+    
+    home_page.num1.click()
+    assert home_page.result_typed.text == '1'
+    assert home_page.result_tab.text == ''
+
+    home_page.plus_btn.click()
+    assert home_page.result_typed.text == '1+'
+    assert home_page.result_tab.text == ''
+
+    home_page.num2.click()
+    assert home_page.result_typed.text == '1+2'
+    assert home_page.result_tab.text == '3'
+
+    home_page.equal_btn.click()
+    assert home_page.result_typed.text == '3'
+    assert home_page.result_tab.text == ''
